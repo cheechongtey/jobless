145c4b1e8790953615df1cff1afd9a33c9e4cb84
Chee Chong <cheechong.tey@sgcarmart.com>
2026-01-28 23:20:15 +0800

    refactor: simplify ESLint command in lint-staged configuration for improved reliability

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,16 +1,17 @@
-/* eslint-disable @typescript-eslint/no-require-imports */
 // Note: In this repo we run ESLint directly (see package.json "lint": "eslint").
 // Some Next.js versions/configs don't support `next lint --fix`, so lint-staged
 // uses `eslint --fix` for reliability.
 
-const path = require('path');
+// const path = require('path');
 
 // see: https://nextjs.org/docs/app/building-your-application/configuring/eslint#lint-staged
-const buildEslintCommand = (filenames) =>
-  `next lint --fix --file ${filenames
-    // need to use __dirname instead of process.cwd() because lint-staged runs from the root of the repo
-    .map((f) => path.relative(__dirname, f))
-    .join(' --file ')}`;
+// const buildEslintCommand = (filenames) =>
+//   `eslint --fix --file ${filenames
+//     // need to use __dirname instead of process.cwd() because lint-staged runs from the root of the repo
+//     .map((f) => path.relative(__dirname, f))
+//     .join(' --file ')}`;
+
+const buildEslintCommand = () => `eslint --fix`;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand],
